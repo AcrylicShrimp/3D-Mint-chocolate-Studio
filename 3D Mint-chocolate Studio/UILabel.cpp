06f4106512ba6a4823abeb503b2decec93025c53
AcrylicShrimp <led789zxpp@naver.com>
2017-05-19 19:22:49 +0900

    Moved all files.

--- 3D Mint-chocolate Studio/UILabel.cpp
+++ 3D Mint-chocolate Studio/UILabel.cpp
@@ -8,32 +8,98 @@
 
 namespace D3MCS::UI
 {
-	UILabel::UILabel(int32_t nNewX, int32_t nNewY, uint32_t nNewWidth, uint32_t nNewHeight)
+	UILabel::UILabel(int32_t nNewX, int32_t nNewY, uint32_t nNewWidth, uint32_t nNewHeight) :
+		UIElement{nNewX, nNewY, nNewWidth, nNewHeight},
+		sFont{GlyphManager::NullFont},
+		nFontSize{0u}
 	{
+		INIT_CONTEXT;
+	}
 
-	}
-	
-	UILabel::UILabel(UILabel &&sSrc)
+	UILabel::UILabel(int32_t nNewX, int32_t nNewY, uint32_t nNewWidth, uint32_t nNewHeight, const char32_t *pContent) :
+		UIElement{nNewX, nNewY, nNewWidth, nNewHeight},
+		sFont{GlyphManager::NullFont},
+		nFontSize{0u},
+		sContent{pContent}
 	{
-		//TODO : Place your implementation of move constructor here.
-		
+		INIT_CONTEXT;
 	}
-	
+
+	UILabel::UILabel(UILabel &&sSrc) :
+		UIElement{std::move(sSrc)},
+		sShader{std::move(sSrc.sShader)},
+		sShaderInput{std::move(sSrc.sShaderInput)},
+		sVertexBuffer{std::move(sSrc.sVertexBuffer)},
+		sTextureBuffer{std::move(sSrc.sTextureBuffer)},
+		sFont{sSrc.sFont},
+		nFontSize{sSrc.nFontSize},
+		sContent{std::move(sSrc.sContent)}
+	{
+		//Empty.
+	}
+
 	UILabel::~UILabel()
 	{
-		//TODO : Place your implementation of destructor here.
-		
+		FIN_CONTEXT;
 	}
 
 	UILabel &UILabel::operator=(UILabel &&sSrc)
 	{
-		if(&sSrc == this)
+		if (&sSrc == this)
 			return *this;
-		
-		//TODO : Place your implementation of move assignment operator here.
-		
-		
+
+		this->UIElement::operator=(std::move(sSrc));
+
+		this->sShader = std::move(sSrc.sShader);
+		this->sShaderInput = std::move(sSrc.sShaderInput);
+		this->sVertexBuffer = std::move(sSrc.sVertexBuffer);
+		this->sTextureBuffer = std::move(sSrc.sTextureBuffer);
+		this->sFont = sSrc.sFont;
+		this->nFontSize = sSrc.nFontSize;
+		this->sContent = std::move(sSrc.sContent);
+
 		return *this;
+	}
+
+	void UILabel::setFont(Font sNewFont, uint32_t nNewFontSize)
+	{
+		this->sFont = sNewFont;
+		this->nFontSize = nNewFontSize;
+
+		//TODO : Decrease the reference count of the last font and size here.
+
+		GlyphManager::instance().bakeString(this->sFont, this->nFontSize, this->sContent.c_str());
+	}
+
+	void UILabel::setContent(const char32_t pContent)
+	{
+		this->sContent = pContent;
+
+		//TODO : Decrease the reference count of the last font and size here.
+
+		GlyphManager::instance().bakeString(this->sFont, this->nFontSize, this->sContent.c_str());
+
+		if (!Render::OpenGLManager::instance().isContextCreated())
+			return;
+
+		auto pGlyphState = GlyphManager::instance().findGlyphState(this->sFont, this->nFontSize);
+
+		if (!pGlyphState)
+			return;
+
+		float nCurrentX{this->nX};
+		float nCurrentY{this->nY};
+		auto iEnd{pGlyphState->sGlyphRenderStateLookup.cend()};
+
+		for (auto nCharacter : this->sContent)
+		{
+			auto iIndex{pGlyphState->sGlyphRenderStateLookup.find(nCharacter)};
+
+			if (iIndex == iEnd)
+				continue;
+
+
+		}
 	}
 
 	void UILabel::render()
@@ -43,11 +109,19 @@ namespace D3MCS::UI
 
 	void UILabel::onContextCreated()
 	{
+		this->sShader.init();
 
+
+		this->sShaderInput.init();
+		this->sVertexBuffer.init();
+		this->sTextureBuffer.init();
 	}
 
 	void UILabel::onContextDestroying()
 	{
-
+		this->sShader.fin();
+		this->sShaderInput.fin();
+		this->sVertexBuffer.fin();
+		this->sTextureBuffer.fin();
 	}
 }
--- 3D Mint-chocolate Studio/UILabel.h
+++ 3D Mint-chocolate Studio/UILabel.h
@@ -9,6 +9,7 @@
 #define _CLASS_D3MCS_UI_LAYOUT_UILABEL_H
 
 #include "Container.h"
+#include "GlyphManager.h"
 #include "OpenGLManager.h"
 #include "Shader.h"
 #include "ShaderInput.h"
@@ -16,6 +17,7 @@
 #include "UIElement.h"
 #include "VideoBuffer.h"
 
+#include <cstdint>
 #include <string>
 #include <utility>
 
@@ -28,10 +30,13 @@ namespace D3MCS::UI
 		Utility::Container<Render::ShaderInput> sShaderInput;
 		Utility::Container<Render::VideoBuffer> sVertexBuffer;
 		Utility::Container<Render::VideoBuffer> sTextureBuffer;
+		Font sFont;
+		uint32_t nFontSize;
 		std::u32string sContent;
 		
 	public:
 		UILabel(int32_t nNewX, int32_t nNewY, uint32_t nNewWidth, uint32_t nNewHeight);
+		UILabel(int32_t nNewX, int32_t nNewY, uint32_t nNewWidth, uint32_t nNewHeight, const char32_t *pContent);
 		UILabel(const UILabel &sSrc) = delete;
 		UILabel(UILabel &&sSrc);
 		~UILabel();
@@ -41,10 +46,19 @@ namespace D3MCS::UI
 		UILabel &operator=(UILabel &&sSrc);
 		
 	public:
+		inline const std::u32string &content() const;
+
+		void setFont(Font sNewFont, uint32_t nNewFontSize);
+		void setContent(const char32_t pContent);
 		virtual void render() override;
 		virtual void onContextCreated() override;
 		virtual void onContextDestroying() override;
 	};
+
+	inline const std::u32string & UILabel::content() const
+	{
+		return this->sContent;
+	}
 }
 
 #endif
